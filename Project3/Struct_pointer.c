97de38a1db216176f9abbe4adb96d2c94c4b1f55
bumsu <hotsem8@gmail.com>
2022-04-06 00:18:12 +0900

    구조체 참조 코드 완성

--- Project3/Struct_pointer.c
+++ Project3/Struct_pointer.c
@@ -14,6 +14,15 @@ int main() {
 	// 화살표 연산자로 구조체 멤버에 접근하여 값 할당
 	strcpy(p1->name, "홍길동");
 	p1->age = 30;
+	strcpy(p1->address, "서울시 용산구 한남동");
+
+	//화살표 연산자로 구조체 멤버에 접근하여 값 출력
+	printf("이름: %s\n", p1->name);		// 홍길동
+	printf("나이: %d\n", p1->age);		// 30
+	printf("주소: %s\n", p1->address);	// 서울시 용산구 한남동
+
+	free(p1);		// 동적 메모리 해제
+	return 0;
 
 
 }
--- Project3/소스.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main() {
-	printf("안녕");
-	printf("캉글");    // 한글 테스트 파일
-}
